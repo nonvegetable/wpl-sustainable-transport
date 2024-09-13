9be65d188cd649b38afd6ebca1c692d2a15dfadf
nonvegetable <100308933+nonvegetable@users.noreply.github.com>
2024-09-13 14:39:39 +0530

    form validation

--- frontend/electric_vehicle/electric_vehicle_script.js
+++ frontend/electric_vehicle/electric_vehicle_script.js
@@ -1,21 +1,27 @@
 function validateQuizForm() {
     let q1Answered = false;
     let q2Answered = false;
+    let q1Correct = false;
+    let q2Correct = false;
 
-    // Check if question 1 is answered
     let q1Options = document.getElementsByName("q1");
     for (let i = 0; i < q1Options.length; i++) {
         if (q1Options[i].checked) {
             q1Answered = true;
+            if (q1Options[i].value === "a") {
+                q1Correct = true;
+            }
             break;
         }
     }
 
-    // Check if question 2 is answered
     let q2Options = document.getElementsByName("q2");
     for (let i = 0; i < q2Options.length; i++) {
         if (q2Options[i].checked) {
             q2Answered = true;
+            if (q2Options[i].value === "b") {
+                q2Correct = true;
+            }
             break;
         }
     }
@@ -26,6 +32,20 @@ function validateQuizForm() {
         return false;
     }
 
+    // Check correctness of answers
+    if (q1Correct) {
+        alert("Correct answer for question 1: " + q1Options[i].nextElementSibling.innerHTML);
+    } else {
+        alert("Incorrect answer for question 1: " + q1Options[i].nextElementSibling.innerHTML);
+    }
+
+    if (q2Correct) {
+        alert("Correct answer for question 2: " + q2Options[i].nextElementSibling.innerHTML);
+    } else {
+        alert("Incorrect answer for question 2: " + q2Options[i].nextElementSibling.innerHTML);
+    }
+
     // If all questions are answered, allow form submission
+    alert("Quiz submitted successfully!");
     return true;
 }
