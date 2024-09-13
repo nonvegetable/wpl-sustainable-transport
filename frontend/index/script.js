03e2beabf61881b48038f95a45186549c2309240
nonvegetable <100308933+nonvegetable@users.noreply.github.com>
2024-09-13 11:07:22 +0530

    added images, navbar, classnames, logo

--- frontend/index/script.js
+++ frontend/index/script.js
@@ -1,10 +1,11 @@
 // Image slider functionality
 const sliderImages = document.querySelectorAll('.slider-image');
-let currentImageIndex = 0
+let currentImageIndex = 0;
+
 function showNextImage() {
     sliderImages[currentImageIndex].classList.remove('active');
     currentImageIndex = (currentImageIndex + 1) % sliderImages.length;
     sliderImages[currentImageIndex].classList.add('active');
+}
 
 setInterval(showNextImage, 5000); // Change image every 5 seconds
-}
